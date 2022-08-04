58ad4d89ce6b21fcd1f37e66acac0c716a9e6864
Angela <angela.cheng@jpl.nasa.gov>
2022-08-04 14:44:09 -0700

    8/4 during meeting

--- opera_coverage/coverage.py
+++ opera_coverage/coverage.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import numpy as np
 from . import formatting as f
 from . import search as s
-from . import plotting as p
+# from . import plotting as p
 
 def get_coverage(sensor: List[str], aoi: Polygon, date: List[datetime] = None) -> List[dict]:
     """
@@ -12,15 +12,15 @@ def get_coverage(sensor: List[str], aoi: Polygon, date: List[datetime] = None) -
     AOI: enter coordinates as Polygon object
     date: leave as none if searching today, else enter time range as datetime tuple: datetime(YYYY,MM,DD)
     """
-    freq = {}
-    next_acq = {}
-    area = {}
+    # freq = {}
+    # next_acq = {}
+    # area = {}
     dataframes = []
     
     for sensor_name in sensor:
-        freq[sensor_name] = ''
-        next_acq[sensor_name] = ''
-        area[sensor_name] = ''
+        # freq[sensor_name] = ''
+        # next_acq[sensor_name] = ''
+        # area[sensor_name] = ''
         
         if 'landsat8' in sensor_name.lower():
             results = s.hls_search('landsat8', aoi, date)
@@ -34,30 +34,30 @@ def get_coverage(sensor: List[str], aoi: Polygon, date: List[datetime] = None) -
         
         dataframes.append(df)
 
-        # return cadence as string or list using get_cadence
-        freq[sensor_name] = s.get_cadence(df)
+        # # return cadence as string or list using get_cadence
+        # freq[sensor_name] = s.get_cadence(df)
         
-        # find next acquisition time, if search time is today then returns 'N/A'
-        if date == None:
-            next_acq[sensor_name] = 'N/A'
+        # # find next acquisition time, if search time is today then returns 'N/A'
+        # if date == None:
+        #     next_acq[sensor_name] = 'N/A'
             
-        else:
-            next_acq[sensor_name] = 'Time of next acquisition after ' + str(date[1]) + ' is ' + str(s.acq_search(sensor_name.lower(), aoi, date[1]))
+        # else:
+        #     next_acq[sensor_name] = 'Time of next acquisition after ' + str(date[1]) + ' is ' + str(s.acq_search(sensor_name.lower(), aoi, date[1]))
         
-        # find intersection of acquisitions from each sensor separately
-        if len(results) == 0:
-            area[sensor_name] = 0
-        else:
-            area[sensor_name] = df.geometry[0]
+        # # find intersection of acquisitions from each sensor separately
+        # if len(results) == 0:
+        #     area[sensor_name] = 0
+        # else:
+        #     area[sensor_name] = df.geometry[0]
             
-            if len(results) > 1:
+        #     if len(results) > 1:
                 
-                for i in range(len(df) - 1):
-                    area[sensor_name] = area[sensor_name].intersection(df.geometry[i + 1])
+        #         for i in range(len(df) - 1):
+        #             area[sensor_name] = area[sensor_name].intersection(df.geometry[i + 1])
 
-    master = s.get_sensor_cadence(dataframes)
-
-    return freq, next_acq, area, master
+    coverage_df = s.get_sensor_cadence(dataframes)
+    return coverage_df
+    # return freq, next_acq, area, master
 
 def get_global_coverage(sensor: List[str], gridsize, date: List[datetime]) -> np.array:
     long_block = np.linspace(-180, 180, gridsize[0] + 1)
@@ -71,16 +71,13 @@ def get_global_coverage(sensor: List[str], gridsize, date: List[datetime]) -> np
             for sensor_name in sensor:    
                 if 'landsat8' in sensor_name.lower():
                     results = s.hls_search('landsat8', aoi, date)
-                    # df = f.format_results_for_hls(results,'landsat8')
                 elif 'sentinel1' in sensor_name.lower():
                     results = s.asf_search(aoi, date)
-                    # df = f.format_results_for_sent1(results)
                 elif 'sentinel2' in sensor_name.lower():
                     results = s.hls_search('sentinel2', aoi, date)
-                    # df = f.format_results_for_hls(results,'sentinel2')
 
                 value[j][i] += len(results)
 
-    p.heatmap(value, long_block, lat_block)
+    # p.heatmap(value, long_block, lat_block)
 
     return value
